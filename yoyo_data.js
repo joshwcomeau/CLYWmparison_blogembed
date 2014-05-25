f4da07c544fa732019f1751b2ba789999441cf34
Josh Comeau <joshwcomeau@gmail.com>
2014-05-25 14:01:26 -0400

    Added 5 more yoyos to 'database'. Played with the sizing for it to fit properly.

--- yoyo_data.js
+++ yoyo_data.js
@@ -1,12 +1,21 @@
 var yoyos = 
 [
   {
-    model:        "Arctic Circle 2",
-    img_url:      "ac2_compact.png",
-    color_hex:    "#0072A6",
-    diameter_mm:  55,
-    width_mm:     45.25,
-    weight_g:     65.1,
+    model:        "Wooly Marmot 2",
+    img_url:      "woolymarmot2_compact.png",
+    color_hex:    "#313340",
+    diameter_mm:  51.67,
+    width_mm:     44.8,
+    weight_g:     65.4,
+    family:       'current'
+  },
+  {
+    model:        "Puffin 2",
+    img_url:      "puffin2_compact.png",
+    color_hex:    "#B300F2",
+    diameter_mm:  53.3,
+    width_mm:     44.3,
+    weight_g:     65.7,
     family:       'current'
   },
   {
@@ -28,23 +37,60 @@ var yoyos =
     family:       'current'
   },
   {
-    model:        "Puffin 2",
-    img_url:      "puffin2_compact.png",
-    color_hex:    "#B300F2",
-    diameter_mm:  53.3,
-    width_mm:     44.3,
-    weight_g:     65.7,
+    model:        "Arctic Circle 2",
+    img_url:      "ac2_compact.png",
+    color_hex:    "#0072A6",
+    diameter_mm:  55,
+    width_mm:     45.25,
+    weight_g:     65.1,
     family:       'current'
   },
   {
-    model:        "Wooly Marmot 2",
-    img_url:      "woolymarmot2_compact.png",
-    color_hex:    "#313340",
-    diameter_mm:  51.67,
-    width_mm:     44.8,
-    weight_g:     65.4,
+    model:        "Yeti",
+    img_url:      "yeti_compact.png",
+    color_hex:    "#ABA965",
+    diameter_mm:  58.2,
+    width_mm:     44.6,
+    weight_g:     69.3,
     family:       'current'
   },
+  {
+    model:        "Bear Vs. Man Round 2",
+    img_url:      "BVM2_compact.png",
+    color_hex:    "#F21612",
+    diameter_mm:  56.5,
+    width_mm:     44.12,
+    weight_g:     66.2,
+    family:       'current'
+  },
+  {
+    model:        "Summit",
+    img_url:      "summit_compact.png",
+    color_hex:    "#7BBF00",
+    diameter_mm:  55.5,
+    width_mm:     46,
+    weight_g:     66.75,
+    family:       'current'
+  },
+  {
+    model:        "Avalanche",
+    img_url:      "avalanche_compact.png",
+    color_hex:    "#AB007C",
+    diameter_mm:  55,
+    width_mm:     42.9,
+    weight_g:     65.9,
+    family:       'current'
+  },
+  {
+    model:        "Sasquatch",
+    img_url:      "sasquatch_compact.png",
+    color_hex:    "#00A693",
+    diameter_mm:  55.6,
+    width_mm:     43,
+    weight_g:     68,
+    family:       'current'
+  }
+
 
 ]
 
